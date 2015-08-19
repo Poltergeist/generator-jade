4b7e3ca3bc556e93c1da9ba2ee6e2641c75d09a0
Sp3c1 <spectator.networx@gmail.com>
2015-08-19 09:55:08 +0200

    feat(jade): Generate jade files recursive.
    Now Jade files get generated recursivly except files starting
    with an underscore.
    
    close #31

--- app/templates/tasks/jade.js
+++ app/templates/tasks/jade.js
@@ -2,13 +2,30 @@
  * grunt-contirb-jade options
  * @type {Object}
  */
+var fs = require('fs'),
+  path = require('path'),
+  walk = function(dir, directory) {
+    var results = [];
+    directory = directory || '';
+    fs.readdirSync(dir).forEach(function(file) {
+      var absolutePath = path.resolve(dir, file),
+        relativePath = path.join(directory, file),
+        stat = fs.statSync(absolutePath);
+      if (stat && stat.isDirectory()) {
+        results = results.concat(walk(absolutePath, relativePath));
+      } else if(/^(?!_).*/g.test(file)) {
+        results.push(relativePath);
+      }
+    });
+    return results;
+  };
 
 module.exports = {
   html: {
     files: [{
       expand: true,
       cwd: '<%= folders.app %>/jade',
-      src: ['index.jade'],
+      src: walk(path.resolve(__dirname, '../app/jade')),
       dest: '.tmp/',
       ext: '.html'
     }],
